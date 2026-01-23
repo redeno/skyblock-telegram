2fbeea556d61c851c04c0887f358c78f93dc093c
redeno <60550304+redeno@users.noreply.github.com>
2026-01-23 16:28:19 +0300

    Update game.js

--- game.js
+++ game.js
@@ -434,8 +434,8 @@ const game = {
                 minions: this.state.minions,
                 pets: this.state.pets,
                 buffs: this.state.buffs,
-                farmingTalents: JSON.stringify(this.state.farmingTalents),
-                farmingQuests: JSON.stringify(this.state.farmingQuests)
+                farmingTalents: this.state.farmingTalents,
+                farmingQuests: this.state.farmingQuests
             }, { onConflict: 'telegram_id' });
         if (error) console.error('Ошибка сохранения:', error);
     },
